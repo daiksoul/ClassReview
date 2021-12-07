c47ddef2011d1bd07af44440234449f83a234c78
GwonYuri <21900048@handong.edu>
2021-12-08 01:03:49 +0900

    로그인 체크 기능 구현 완료

--- src/main/java/com/myreview/app/user/UserServiceImpl.java
+++ src/main/java/com/myreview/app/user/UserServiceImpl.java
@@ -2,8 +2,10 @@ package com.myreview.app.user;
 
 @Service
 public class UserServiceImpl {
+	
 	@Autowired
 	UserDAO userDAO;
+	
 	public UserVO getUser(UserVO vo) {
 		return userDAO.getUser(vo);
 	}
