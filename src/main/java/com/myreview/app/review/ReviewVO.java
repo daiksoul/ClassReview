a97106de94cbf6d948e1fb2cd8fac8703275a9e0
DAIK <daiksoul@gmail.com>
2021-12-07 22:18:51 +0900

    mybatis
    mybatis 설정이랑 머시기 어쩌구

---
+++ src/main/java/com/myreview/app/review/ReviewVO.java
@@ -0,0 +1,16 @@
+package com.myreview.app.review;
+
+import java.sql.Timestamp;
+
+public class ReviewVO {
+	int id;
+	String title;
+	String professor;
+	String content;
+	Timestamp regdate;
+	int rating;
+	int semester;
+	int author;
+	
+	
+}
